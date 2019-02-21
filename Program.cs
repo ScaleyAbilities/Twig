bfd93be8be8c96a26f43ee9e8687eeb27c76d9e2
Michael Baart <michael.baart96@gmail.com>
2019-02-20 22:05:21 -0800

    Add comment for logic

--- Program.cs
+++ Program.cs
@@ -14,6 +14,11 @@ namespace Twig
             var dt = new DataTable();
             dt = TriggerHelper.GetTriggers();
 
+            // if time of tansaction is older than 60 seconds:
+            //      updated stock price
+            //      check that user has enough stocks to sell
+            //       or check if they have enough money to buy
+            //      set db row type from trigger to pending
             foreach(DataRow row in dt.Rows){
                 // if (row['transactiontime'] 
                 // var price = TriggerHelper.UpdateStockPrice(stockSymbol);
